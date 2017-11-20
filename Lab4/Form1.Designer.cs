3ffd0ffdbf93810f5ff33af78a10420c0133a152
tmahan95 <tmahan95@users.noreply.github.com>
2017-11-20 13:39:47 -0500

    lab4
    lab 4

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Lab4
+{
+    partial class lab4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpaTxtBox = new System.Windows.Forms.TextBox();
+            this.testScoreTxtBox = new System.Windows.Forms.TextBox();
+            this.gpaLbl = new System.Windows.Forms.Label();
+            this.testScorelbl = new System.Windows.Forms.Label();
+            this.ttlAcceptDispLbl = new System.Windows.Forms.Label();
+            this.ttlAcceptedLbl = new System.Windows.Forms.Label();
+            this.ttlRejectedLbl = new System.Windows.Forms.Label();
+            this.ttlRejectDispLbl = new System.Windows.Forms.Label();
+            this.enterSubBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gpaTxtBox
+            // 
+            this.gpaTxtBox.Location = new System.Drawing.Point(28, 36);
+            this.gpaTxtBox.Name = "gpaTxtBox";
+            this.gpaTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.gpaTxtBox.TabIndex = 0;
+            // 
+            // testScoreTxtBox
+            // 
+            this.testScoreTxtBox.Location = new System.Drawing.Point(161, 36);
+            this.testScoreTxtBox.Name = "testScoreTxtBox";
+            this.testScoreTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.testScoreTxtBox.TabIndex = 1;
+            // 
+            // gpaLbl
+            // 
+            this.gpaLbl.AutoSize = true;
+            this.gpaLbl.Location = new System.Drawing.Point(62, 20);
+            this.gpaLbl.Name = "gpaLbl";
+            this.gpaLbl.Size = new System.Drawing.Size(29, 13);
+            this.gpaLbl.TabIndex = 2;
+            this.gpaLbl.Text = "GPA";
+            // 
+            // testScorelbl
+            // 
+            this.testScorelbl.AutoSize = true;
+            this.testScorelbl.Location = new System.Drawing.Point(188, 20);
+            this.testScorelbl.Name = "testScorelbl";
+            this.testScorelbl.Size = new System.Drawing.Size(59, 13);
+            this.testScorelbl.TabIndex = 3;
+            this.testScorelbl.Text = "Test Score";
+            // 
+            // ttlAcceptDispLbl
+            // 
+            this.ttlAcceptDispLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ttlAcceptDispLbl.Location = new System.Drawing.Point(28, 100);
+            this.ttlAcceptDispLbl.Name = "ttlAcceptDispLbl";
+            this.ttlAcceptDispLbl.Size = new System.Drawing.Size(100, 23);
+            this.ttlAcceptDispLbl.TabIndex = 4;
+            // 
+            // ttlAcceptedLbl
+            // 
+            this.ttlAcceptedLbl.AutoSize = true;
+            this.ttlAcceptedLbl.Location = new System.Drawing.Point(37, 78);
+            this.ttlAcceptedLbl.Name = "ttlAcceptedLbl";
+            this.ttlAcceptedLbl.Size = new System.Drawing.Size(80, 13);
+            this.ttlAcceptedLbl.TabIndex = 5;
+            this.ttlAcceptedLbl.Text = "Total Accepted";
+            // 
+            // ttlRejectedLbl
+            // 
+            this.ttlRejectedLbl.AutoSize = true;
+            this.ttlRejectedLbl.Location = new System.Drawing.Point(170, 78);
+            this.ttlRejectedLbl.Name = "ttlRejectedLbl";
+            this.ttlRejectedLbl.Size = new System.Drawing.Size(77, 13);
+            this.ttlRejectedLbl.TabIndex = 6;
+            this.ttlRejectedLbl.Text = "Total Rejected";
+            // 
+            // ttlRejectDispLbl
+            // 
+            this.ttlRejectDispLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ttlRejectDispLbl.Location = new System.Drawing.Point(161, 100);
+            this.ttlRejectDispLbl.Name = "ttlRejectDispLbl";
+            this.ttlRejectDispLbl.Size = new System.Drawing.Size(100, 23);
+            this.ttlRejectDispLbl.TabIndex = 7;
+            // 
+            // enterSubBtn
+            // 
+            this.enterSubBtn.Location = new System.Drawing.Point(104, 152);
+            this.enterSubBtn.Name = "enterSubBtn";
+            this.enterSubBtn.Size = new System.Drawing.Size(75, 35);
+            this.enterSubBtn.TabIndex = 2;
+            this.enterSubBtn.Text = "Submit";
+            this.enterSubBtn.UseVisualStyleBackColor = true;
+            this.enterSubBtn.Click += new System.EventHandler(this.enterSubBtn_Click);
+            // 
+            // lab4
+            // 
+            this.AcceptButton = this.enterSubBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.enterSubBtn);
+            this.Controls.Add(this.ttlRejectDispLbl);
+            this.Controls.Add(this.ttlRejectedLbl);
+            this.Controls.Add(this.ttlAcceptedLbl);
+            this.Controls.Add(this.ttlAcceptDispLbl);
+            this.Controls.Add(this.testScorelbl);
+            this.Controls.Add(this.gpaLbl);
+            this.Controls.Add(this.testScoreTxtBox);
+            this.Controls.Add(this.gpaTxtBox);
+            this.Name = "lab4";
+            this.Text = "Lab4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox gpaTxtBox;
+        private System.Windows.Forms.TextBox testScoreTxtBox;
+        private System.Windows.Forms.Label gpaLbl;
+        private System.Windows.Forms.Label testScorelbl;
+        private System.Windows.Forms.Label ttlAcceptDispLbl;
+        private System.Windows.Forms.Label ttlAcceptedLbl;
+        private System.Windows.Forms.Label ttlRejectedLbl;
+        private System.Windows.Forms.Label ttlRejectDispLbl;
+        private System.Windows.Forms.Button enterSubBtn;
+    }
+}
+
---
+++ Lab4/Form1.cs
@@ -0,0 +1,87 @@
+﻿//Timothy Mahan
+//Lab4
+//Due: 2/16/16
+//Section: 01
+/*Program takes the input GPA and test score and tests it to see if the applicant should be accepted or rejected.
+ If the GPA is at least a 3 and the test score is at least a 60, the applicant is accepted. If the GPA is below 3, and
+ the test score is an 80 or greater, the applicant is accepted, otherwise the applicant is rejected.*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class lab4 : Form
+    {
+        private const byte APP_ACCEPTED = 1;//Field variable to add 1 to totalAccepted variable 
+        private const byte APP_REJECTED = 1;//Field variable to add 1 to totalRejected variable
+        private ushort totalAccepted = 0; //Field variable to count accepted applications
+        private ushort totalRejected = 0; //Field variable to count rejected application
+
+
+        public lab4()
+        {
+            InitializeComponent();
+        }
+
+        private void enterSubBtn_Click(object sender, EventArgs e)
+        {
+            double gpaVar = 0; //Declare gpaVar variable that will hold the GPA entered by user
+            uint testScore = 0; //Declare testScore variable that will hold the testScore entered by user
+
+            if (double.TryParse(gpaTxtBox.Text, out gpaVar))//Convert gpa text string to double and put in gpa double variable
+            {
+                if (uint.TryParse(testScoreTxtBox.Text, out testScore)) //Convert test score to double and placed in test Score variable
+                {
+                    if (gpaVar >= 3 && testScore >= 60)//If GPA is greater than 3, accepts application, adds APP_ACCEPTED to totalRejected.
+                    {
+                        totalAccepted += APP_ACCEPTED;
+                        MessageBox.Show("Accept");
+                    }
+                    else
+                    {
+                        if (testScore >= 80)//If test score is greater than 80, accepts application, adds APP_ACCEPTED to totalRejected
+                        {
+                            totalAccepted += APP_ACCEPTED;
+                            MessageBox.Show("Accept");
+                        }
+
+
+                        else
+                        {
+                            totalRejected += APP_REJECTED;//If gpa is less than 3 and testscore is less than 80, add APP_REJECTED to totalREJECTED
+                            MessageBox.Show("Reject");
+
+                            
+
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Invalid data entered in the Test Score box, please enter whole numbers ONLY.");
+                
+                }
+                
+        }
+            else
+            {
+                MessageBox.Show("Invalid data entered in the GPA box, please enter numbers ONLY");
+            }
+            ttlAcceptDispLbl.Text = totalAccepted.ToString();
+            ttlRejectDispLbl.Text = totalRejected.ToString();
+        }
+    }
+}
+            
+        
+    
